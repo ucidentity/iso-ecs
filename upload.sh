081c8237938375c7447a1661cec7288dad822918
Mike Jones <mikejones@security.berkeley.edu>
2022-01-05 15:36:39 -0800

    script to upload component templates to an elasticsearch server

---
+++ upload.sh
@@ -0,0 +1,13 @@
+
+# Usage:  ./upload.sh elasticsearch-hostname-or-ip
+
+version="$(cat version)"
+for file in `ls generated/elasticsearch/component/*.json`
+do
+  fieldset=`echo $file | cut -d/ -f4 | cut -d. -f1 | tr A-Z a-z`
+  component_name="ecs_${version}_${fieldset}"
+  api="_component_template/${component_name}"
+
+  # echo "$file => $api"
+  curl  -XPUT "$1:9200/$api" --header "Content-Type: application/json" -d @"$file"
+done
