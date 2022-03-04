19879229a0c73e9ca43670bb730032536fdc9b8f
Mike Jones <mikejones@security.berkeley.edu>
2022-03-04 13:40:57 -0800

    fix upload script to not verify cert

--- upload.sh
+++ upload.sh
@@ -2,12 +2,12 @@
 # Usage:  ./upload.sh elasticsearch-hostname-or-ip
 
 version="$(cat version)"
-for file in `ls generated/elasticsearch/component/*.json`
+for file in `ls generated/elasticsearch/composable/component/*.json`
 do
-  fieldset=`echo $file | cut -d/ -f4 | cut -d. -f1 | tr A-Z a-z`
+  fieldset=`echo $file | cut -d/ -f5 | cut -d. -f1 | tr A-Z a-z`
   component_name="ecs_${version}_${fieldset}"
   api="_component_template/${component_name}"
 
   # echo "$file => $api"
-  curl  -XPUT "$1:9200/$api" --header "Content-Type: application/json" -d @"$file"
+  curl  -k -XPUT "$1:9200/$api" --header "Content-Type: application/json" -d @"$file"
 done
