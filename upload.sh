fbb700eb391e5f7de523e1edb59ea6e4ac2a1c65
Mike Jones <mikejones@berkeley.edu>
2022-03-23 15:37:58 -0700

    prompt for password

--- upload.sh
+++ upload.sh
@@ -1,5 +1,14 @@
 
-# Usage:  ./upload.sh elasticsearch-hostname-or-ip
+# Usage:  ./upload.sh  elasticsearch-hostname-or-ip
+
+# Prompt for username and password
+printf "Username: "
+read USERNAME
+stty -echo
+printf "Password: "
+read PASSWORD
+stty echo
+printf "\n"
 
 version="$(cat version)"
 for file in `ls generated/elasticsearch/composable/component/*.json`
@@ -8,6 +17,8 @@ do
   component_name="ecs_${version}_${fieldset}"
   api="_component_template/${component_name}"
 
-  # echo "$file => $api"
-  curl  -k -XPUT "$1:9200/$api" --header "Content-Type: application/json" -d @"$file"
+
+
+# echo "$file => $api"
+curl  -k -u $USERNAME:$PASSWORD -XPUT "$1:9200/$api" --header "Content-Type: application/json" -d @"$file"
 done
